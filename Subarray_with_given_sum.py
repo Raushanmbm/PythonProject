2d217d4adfcd3ea068b4727fcbf78c92682c9090
Raushanmbm <raushanmbm@gmail.com>
2019-07-17 07:14:16 +0530

    subarray_sum_print_index

---
+++ Subarray_with_given_sum.py
@@ -0,0 +1,29 @@
+def subarray_sum(number, array):
+    start = 0
+    end = 1
+
+    while start < len(array) and end < (len(array) + 1):
+        total = sum(array[start:end])
+        if total < number:
+            end += 1
+            total = sum(array[start:end])
+        if total > number:
+            start += 1
+            total = sum(array[start:end])
+        elif total == number:
+            start += 1
+            return (str(start) + ' ' + str(end))
+        print(total)
+
+    return ('-1')
+
+case=1
+N=[5,12]
+A=[1,2,3,7,5]
+cases = int(input())
+
+for case in range(cases):
+    N = list(map(int, input().split()))
+    N = N[1]
+    A = list(map(int, input().split()))
+    print(subarray_sum(N, A))
